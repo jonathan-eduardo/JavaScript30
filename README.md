6065fae87539247369ef0cf0d383861dd2cba228
Jonathan Silva <jonathanedsilva@gmail.com>
2024-04-04 10:39:37 -0300

    solved first challenge

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript30
+
+This is my version of a [Javascript course](https://javascript30.com) made by [Wes Bos](https://github.com/wesbos). I highly recommend you to take the course. It's completely free and of high quality!
+
+![](https://javascript30.com/images/JS3-social-share.png)
+
+You can find the course repository here: [JavaScript30](https://github.com/wesbos/JavaScript30)
---
+++ challenges/01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,44 @@
+const audios = document.querySelectorAll('audio')
+const volumeView = document.querySelector('.volume-view')
+const volumeInput = document.getElementById('volume')
+const root = document.documentElement
+
+function getRootVariable(name) {
+  return getComputedStyle(root).getPropertyValue(name)
+}
+
+function handleKeyPressed(event) {
+  let keyName = ''
+
+  if (event.type === 'click') {
+    keyName = event.target.dataset.key || event.target.parentElement.dataset.key
+  } else {
+    keyName = event.key.toUpperCase()
+  }
+
+  const key = document.querySelector(`div[data-key="${keyName}"]`)
+  const audio = document.querySelector(`audio[data-key="${keyName}"]`)
+  const transitionTime = getRootVariable('--transition-time-in-seconds')
+
+  if (!audio) return
+  audio.currentTime = 0
+  audio.volume = getCurrentVolume()
+  audio.play()
+  key.classList.add('playing')
+  setTimeout(() => {
+    key.classList.remove('playing')
+  }, transitionTime * 1000)
+}
+
+function getCurrentVolume() {
+  return Number(volumeInput.value) / 100
+}
+
+function updateVolumeView() {
+  console.log(this.value)
+  volumeView.textContent = this.value + '%'
+}
+
+document.addEventListener('keydown', handleKeyPressed)
+document.addEventListener('click', handleKeyPressed)
+volumeInput.addEventListener('input', updateVolumeView)
