7728073e8eec5cecb4f31aeedf302fac6409ff54
Jonathan Silva <jonathanedsilva@gmail.com>
2024-04-09 20:57:08 -0300

    improve javascript logic and fix css spacing property

--- challenges/03 - CSS Variables/main.js
+++ challenges/03 - CSS Variables/main.js
@@ -1,15 +1,13 @@
 const inputs = document.querySelectorAll('input')
 
-inputs.forEach((input) => {
-  input.addEventListener('input', () => {
-    const view = document.querySelector(`.${input.name}-view`)
-    const varName = `--${input.name}`
-    const unit = input.dataset.unit
-    const value = input.value.includes('#')
-      ? input.value
-      : `${input.value}${unit}`
+function handleInput() {
+  const view = document.querySelector(`.${this.name}-view`)
+  const varName = `--${this.name}`
+  const unit = this.dataset.unit || ''
+  const value = `${this.value}${unit}`
 
-    document.body.style.setProperty(varName, value)
-    view.textContent = value
-  })
-})
+  document.documentElement.style.setProperty(varName, value)
+  view.textContent = value
+}
+
+inputs.forEach((input) => input.addEventListener('input', handleInput))
