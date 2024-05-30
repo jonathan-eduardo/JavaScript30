8402358699de0399f5f36dc9e0cf40644c340021
Jonathan Silva <jonathanedsilva@gmail.com>
2024-05-29 22:08:17 -0300

    add challenge 28

---
+++ challenges/28 - Video Speed Controller/main.js
@@ -0,0 +1,17 @@
+const speedControl = document.querySelector('.speed-control')
+const video = document.querySelector('.video')
+
+function handleSpeedControl({ offsetY }) {
+  const speedElement = this.firstElementChild
+  const baseValue = offsetY / this.offsetHeight
+  const speedValue = baseValue * 4
+  const minValue = 0.2
+  if (speedValue >= minValue) {
+    const percentage = Math.floor(baseValue * 100)
+    speedElement.style.height = `${percentage}%`
+    speedElement.innerText = `${speedValue.toFixed(1)}x`
+    video.playbackRate = Number(speedValue.toFixed(1))
+  }
+}
+
+speedControl.addEventListener('mousemove', handleSpeedControl)
